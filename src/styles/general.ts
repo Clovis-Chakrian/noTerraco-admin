027dce1e72147c754032dcc2cfd940053c0edd4f
Clovis Chakrian <clovischakriandev@gmail.com>
2022-01-28 08:03:57 -0300

    Updated the global styles

--- src/styles/general.ts
+++ src/styles/general.ts
@@ -6,6 +6,7 @@ const general = {
   container: {
     flex: 1,
     backgroundColor: colors.primary[0],
+    paddingTop: 10,
   },
 
   label: {
@@ -22,13 +23,53 @@ const general = {
   },
 
   input: {
-    width: Dimensions.get('screen').width -40,
+    width: Dimensions.get('screen').width - 40,
     height: 65,
     borderRadius: 10,
     backgroundColor: colors.secondary[1],
     color: colors.text[0],
     fontFamily: fonts.fontFamily,
     paddingHorizontal: 10
+  },
+
+  text: {
+    fontFamily: fonts.fontFamily,
+    fontSize: fonts.fontSize.text,
+    color: colors.text[0]
+  },
+
+  picker: {
+    width: Dimensions.get('screen').width - 20,
+    color: colors.primary[1],
+    fontFamily: fonts.fontFamilyBold,
+    fontSize: fonts.fontSize.subtitle,
+    height: 45,
+    borderBottomWidth: 1,
+    borderColor: colors.secondary[0],
+    marginBottom: 20
+  },
+
+  pickerItem: {
+    color: colors.primary[1],
+    fontFamily: fonts.fontFamilyBold,
+    fontSize: fonts.fontSize.subtitle
+  },
+
+  imageInput: {
+    width: Dimensions.get('screen').width - 40,
+    height: Dimensions.get('screen').width - 40,
+    borderRadius: 10,
+  },
+
+  multilineInput: {
+    width: Dimensions.get('screen').width - 40,
+    height: 180,
+    borderRadius: 10,
+    backgroundColor: colors.secondary[1],
+    color: colors.text[0],
+    fontFamily: fonts.fontFamily,
+    paddingHorizontal: 10,
+    paddingVertical: 10
   },
 };
 
