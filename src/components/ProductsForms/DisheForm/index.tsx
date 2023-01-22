41b59eb75b7d38e0fb5e1f76f698d2a39f72138a
Clóvis Chakrian <clovischakriandev@gmail.com>
2023-01-22 20:54:11 -0300

    Created components to clean the create and edit product screens

---
+++ src/components/ProductsForms/DisheForm/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+function DisheForm () {
+  return (
+    <View>
+      <Text> Tela IDisheCard </Text>
+    </View>
+  );
+};
+
+export default DisheForm;
